aff4050c182b4c21a30ab6dfb88e04a3477b62f5
pinmacaroon <mark.pridalko@gmail.com>
2025-05-18 13:29:23 +0200

    version change, preparing for deployment

--- src/main/java/com/github/pinmacaroon/dchook/Hook.java
+++ src/main/java/com/github/pinmacaroon/dchook/Hook.java
@@ -30,7 +30,7 @@ public class Hook implements DedicatedServerModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static HttpClient HTTPCLIENT = HttpClient.newHttpClient();
 	public static Gson GSON = new GsonBuilder().create();
-	public static final String VERSION = "0.3.0";
+	public static final String VERSION = "0.3.0-alpha";
 	public static final String DOCS_URL = "https://modrinth.com/mod/dchook";
 	public static final Random RANDOM = new Random(Instant.now().getEpochSecond());
 	public static final Pattern WEBHOOK_URL_PATTERN = Pattern.compile("^https:\\/\\/(ptb\\.|canary\\.)?discord\\.com\\/api\\/webhooks\\/\\d+\\/.+$");
